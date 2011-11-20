971ae386c80c9cc71bb6ba520d6470ddb06c7cca
Rich Dougherty <rich@rd.gen.nz>
2011-11-21 10:11:46 +1300

    Use built-in compiler to create InterpretedFunction

--- src/main/scala/com/richdougherty/jsai/App.scala
+++ src/main/scala/com/richdougherty/jsai/App.scala
@@ -9,6 +9,7 @@ import org.mozilla.javascript.IRFactory
 import org.mozilla.javascript.ErrorReporter
 import org.mozilla.javascript.EvaluatorException
 import org.mozilla.javascript.Interpreter
+import org.mozilla.javascript.InterpreterData
 
 /**
  * @author Rich Dougherty <http://www.richdougherty.com/>
@@ -21,41 +22,15 @@ object App {
     //   http://www.informit.com/guides/content.aspx?g=java&seqNum=562
     val cx = Context.enter()
     try {
+      cx.setOptimizationLevel(-1);
+
       val source = "'Hello '+'world.'"
       val sourceFileName = "HelloWorld"
       val sourceLineNo = 1
 
-      val compilerEnv = new CompilerEnvirons()
-      val errorReporter = new ErrorReporter {
-	    def warning(message: String, sourceName: String, line: Int,
-	                 lineSource: String, lineOffset: Int) = {
-	      // TODO: Do something?
-	    }
-	    def error(message: String, sourceName: String, line: Int,
-	               lineSource: String, lineOffset: Int) = {
-	      throw runtimeError(
-                message, sourceName, line, lineSource, lineOffset);
-	    }
-	    def runtimeError(message: String, sourceName: String,
-	                                    line: Int, lineSource: String,
-	                                    lineOffset: Int): EvaluatorException = {
-	      new EvaluatorException(
-                message, sourceName, line, lineSource, lineOffset)
-	    }
-      }
-      val parser = new Parser(compilerEnv, errorReporter)
-      val ast = parser.parse(source, sourceFileName, sourceLineNo)
-      val irf = new IRFactory(compilerEnv, errorReporter)
-      var tree = irf.transformTree(ast)
-      val compiler = new Interpreter()
-      val bytecode = compiler.compile(compilerEnv,
-                                           tree, tree.getEncodedSource(),
-                                           false)
-      val script = compiler.createScriptObject(bytecode, null)
-      println(script)
-
       val scope = cx.initStandardObjects()
-      val result = cx.evaluateString(scope, source, sourceFileName, sourceLineNo, null)
+      val script = cx.compileString(source, sourceFileName, sourceLineNo, null)
+      val result = script.exec(cx, scope)
       println(result)
     } finally {
       Context.exit()
